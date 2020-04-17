7fd9604eb711149b695921814ec3bf6594c95f41
BuildTools <unconfigured@null.spigotmc.org>
2020-04-17 23:44:36 +0100

    Started squirrel script

---
+++ Assets/Scripts/SquirrelController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquirrelController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    private float accelleration;
+    private float topSpeed;
+    private float decelleration;
+
+    private float currentSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
